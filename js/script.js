f72aca712d4a2136df397211561b6af7d4b2f4a3
Mukhammadjon <muhammadjonabdurashidov2@gmail.com>
2022-07-27 05:16:14 +0500

    git add

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var alert_info = alert("xush kelibsiz");
+
+var name = prompt("ismingiz" , "Muhammadjon");
+
+var your_last_name = prompt("familiyangiz" , "Abdurashidov")
+
+var address = prompt("qayerliksiz" , "andijon")
+
+var your_profession = prompt("kasbingiz" , "dasturchi");
+
+var info_confirm = confirm("rahmat .^.");
+
+
+console.log(`foydalanuvchining ismi: ${name}. foydalanuvchining familiyasi: ${your_last_name}.           yashash joyingiz: ${address}.                                              kasbingiz: ${your_profession}`);
